c8751d83f0eac1474039b6d0572230898f73ac26
Jesse Himmelstein <jesse@heavenstone.net>
2013-02-12 12:44:15 +0100

    added rotation

--- platform/site/optics/actions.js
+++ platform/site/optics/actions.js
@@ -4,8 +4,8 @@
       "backgroundColor": "black"
     },
     update: function() {
-      canvas = $("#gameCanvas");
-      context = canvas[0].getContext("2d");
+      var canvas = $("#gameCanvas");
+      var context = canvas[0].getContext("2d");
       context.setFillColor(this.params.backgroundColor);
       context.fillRect(0, 0, canvas.width(), canvas.height());
     }
@@ -18,8 +18,8 @@
       y: 0
     },
     update: function() {
-      canvas = $("#gameCanvas");
-      context = canvas[0].getContext("2d");
+      var canvas = $("#gameCanvas");
+      var context = canvas[0].getContext("2d");
       context.drawImage(this.params.image, this.params.x, this.params.y);
     }
   },
@@ -32,13 +32,22 @@
     paramDefs: {
       type: null,
       row: 0,
-      col: 0
+      col: 0,
+      rotation: 0
     },
     update: function() {
       // This could be done by drawImage() if better expressions existed
-      canvas = $("#gameCanvas");
-      context = canvas[0].getContext("2d");
-      context.drawImage(this.assets[this.params.type], this.params.row * 52 + 33, this.params.col * 52 + 33);
+      var canvas = $("#gameCanvas");
+      var context = canvas[0].getContext("2d");
+      var x = this.params.row * 52 + 33 + 26;
+      var y = this.params.col * 52 + 33 + 26;
+
+      context.save();
+      context.translate(x, y);
+      // Convert to radians
+      context.rotate(this.params.rotation * Math.PI / 180);
+      context.drawImage(this.assets[this.params.type], -26, -26);
+      context.restore();
     }
   },
 
