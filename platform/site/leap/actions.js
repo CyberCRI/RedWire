9bdb8b66e91663a3d6272876834c76bf7071f411
drpepper <jesse@heavenstone.net>
2013-08-09 12:28:20 +0200

    started adding leap game

---
+++ platform/site/leap/actions.js
@@ -0,0 +1,192 @@
+({
+  doInParallel: { 
+    doc: "Just to place children under it"
+  },
+
+  changeParameterThroughKeyboard: {
+    paramDefs: {
+      "parameter": { direction: "inout" },
+      "keysDown": null,
+      "keyMap": null // Keymap must be in a form like { "37": "-10", "39": 3, "41": "hello" }
+    },
+    update: function() {
+      for(var keyCode in this.params.keyMap)
+      {
+        // is the key down?
+        if(this.params.keysDown[keyCode])
+        {
+          // Parse value of keyMap
+          var value = this.params.keyMap[keyCode];
+          if(_.isString(value) && value.length > 0 && (value[0] == "+" || value[0] == "-")) 
+          {
+            // Treat it as a numerical difference
+            this.params.parameter += Number(value);
+          }
+          else
+          {
+            this.params.parameter = value;
+          }
+          // only treat a single keycode
+          break;
+        }
+      } 
+    }
+  },
+
+  doInSequence: {
+    paramDefs: {
+      activeChild: { direction: "inout", default: 0 },
+    },
+    listActiveChildren: function() { return [this.params.activeChild]; },
+    handleSignals: function() { 
+      if(this.signals[this.params.activeChild] == GE.signals.DONE)
+        this.params.activeChild++;
+
+      if(this.params.activeChild > this.children.length - 1)
+      {
+        this.params.activeChild = 0;
+        return GE.signals.DONE;
+      }
+    } 
+  },
+
+  doForSomeTime: {
+    paramDefs: {
+      time: null,
+      timer: { direction: "inout" }
+    },
+    update: function() { 
+      if(this.params.timer++ >= this.params.time) {
+        this.params.timer = 0;
+        return GE.signals.DONE;
+      }
+    }
+  },
+
+  doWhile: {
+    paramDefs: {
+      value: null
+    },
+    update: function() { 
+      if(!this.params.value) return GE.signals.DONE;
+    }
+  },
+
+  when: {
+    paramDefs: {
+      value: null
+    },
+    listActiveChildren: function() { 
+      return this.tools.childByName(this.children, this.params.value);
+    }
+  },
+
+  // Selects the first branch if the expression is truthy, else the second (if it exists)
+  "if": {
+    paramDefs: {
+      value: null
+    },
+    listActiveChildren: function() { 
+      return this.params.value ? [0] : this.children.length > 1 ? [1] : []; 
+    }
+  },
+
+  sandwich: {
+    paramDefs: {
+      condition: null,
+      started: { direction: "inout" }
+    },
+    listActiveChildren: function() { 
+      if(!this.params.track)
+      {
+        if(!this.params.condition) {
+          return [0];
+        } else {
+          this.params.track = true;
+          return [1];
+        }
+      } else {
+        if(this.params.condition) {
+          return [1];
+        } else {
+          this.params.track = false;
+          return [2];
+        }
+      }
+    }
+  },
+
+  detectMouse: {
+    paramDefs: {
+      "shapes": null,
+      "shape": { direction: "inout" },
+      "mousePosition": null,
+      "mouseDown": null,
+      "state": { direction: "inout" },
+      "dragStartPosition": { direction: "inout" },
+      "minimumDragDistance": { default: "5" }
+    },
+    update: function() { 
+      // Implement a state machine, starting at the "none" state
+      if(!this.params.state) this.params.state = "none";
+
+      //this.log(GE.logLevels.INFO, "shapes", this.params.shapes);
+
+      switch(this.params.state) {
+        case "none":
+          if(this.params.mousePosition) {
+            for(var i in this.params.shapes) {
+              if(this.tools.pointIntersectsShape(this.params.mousePosition, this.params.shapes[i])) {
+                this.log(GE.logLevels.INFO, "Entering hover mode. Old state = " + this.params.state);
+                this.params.state = "hover";
+                this.params.shape = this.params.shapes[i];
+                break;
+              }
+            }
+          } 
+          break;
+        case "hover":
+          if(!this.params.mousePosition || !this.tools.pointIntersectsShape(this.params.mousePosition, this.params.shape)) {
+            this.params.state = "none";
+            this.params.shape = null;
+            this.log(GE.logLevels.INFO, "Leaving hover mode")
+          } else if(this.params.mouseDown) {
+            this.params.dragStartPosition = this.params.mousePosition;
+            this.params.state = "pressed";
+            this.log(GE.logLevels.INFO, "Entering presed mode")
+          }
+          break;
+        case "pressed":
+          if(!this.params.mouseDown) {
+            this.params.state = "click";
+            this.params.dragStartPosition = null;
+            this.log(GE.logLevels.INFO, "Leaving pressed mode")
+          } else if(Vector.create(this.params.dragStartPosition).distanceFrom(Vector.create(this.params.mousePosition)) >= this.params.minimumDragDistance) {
+            this.params.state = "startDrag";
+            this.params.dragStartPosition = null;
+            this.log(GE.logLevels.INFO, "Entering drag mode")
+          }
+          break;
+        case "click":
+          this.params.state = "hover";
+          break;
+        case "startDrag":
+          this.params.state = "drag";
+          break;
+        case "drag":
+          if(!this.params.mouseDown) {
+            this.params.state = "endDrag";
+            this.params.dragStartPosition = null;
+            this.log(GE.logLevels.INFO, "Leaving drag mode")
+          }
+          break;
+        case "endDrag":
+          this.params.state = "hover";
+          break;
+        default:
+          throw new Error("Unknown state '" + this.params.state + "'");
+      }
+    }
+  }
+
+});
---
+++ platform/site/leap/tools.js
@@ -0,0 +1,151 @@
+({
+  // Add a shape to be drawn to graphics
+  drawShape: function(shape, oldShapes) 
+  {
+    var shapes = oldShapes || {};
+    shapes[_.uniqueId()] = shape;
+    return shapes; 
+  },
+
+  // Checks if a point intersects a shape.
+  // Currently does not take rotation into account, and only supports circles and rectangles.
+  // Requires Sylvester.js
+  pointIntersectsShape: function(point, shape)
+  {
+    if(!shape.fillStyle && !shape.strokeStyle) return false;
+
+    switch(shape.type)
+    {
+      case "circle":
+        var center = Vector.create(shape.center);
+        if(shape.translation) center = center.add(shape.translation);
+        var lineWidth = shape.lineWidth || 1;
+        var scale = shape.scale || 1;
+        var minDistance = shape.fillStyle ? 0 : shape.radius - lineWidth;
+        var maxDistance = shape.strokeStyle ? shape.radius + lineWidth : shape.radius;
+        var distance = center.distanceFrom(Vector.create(point));
+        return distance >= minDistance * scale && distance <= maxDistance * scale;
+
+      case "rectangle":
+        // Move the point to the frame of the shape
+        var pointInShapeFrame = Vector.create(point);
+        if(shape.translation) pointInShapeFrame = pointInShapeFrame.subtract(shape.translation);
+        return pointInShapeFrame.elements[0] >= shape.position[0] && pointInShapeFrame.elements[0] <= shape.position[0] + shape.size[0] &&
+          pointInShapeFrame.elements[1] >= shape.position[1] && pointInShapeFrame.elements[1] <= shape.position[1] + shape.size[1];
+
+      default:
+        throw new Error("Shape type '" + shape.type + "' is not supported");
+    }
+  },
+
+  //returns a copy of 'tab' without the element at position 'index'
+  removeElement: function(tab, index)
+  {
+    // By all logic, this should work. But after days of testing it, I give up...
+    //   return tab.slice(0, index).concat(tab.slice(index + 1));
+    // Here's the slow but foolproof way
+    var newArray = [];
+    for(var i = 0; i < tab.length; i++) {
+      if(i != index) newArray.push(tab[i]);
+    }
+    return newArray;
+  },
+
+  //converts a pixel coordinate to a board coordinate
+  //assumes that the board is made out of squares
+  toBoardCoordinate: function(pixelCoordinate, upperLeftBoardMargin, cellSize)
+  {
+    var res = Math.floor((pixelCoordinate - upperLeftBoardMargin)/cellSize);
+    return res;
+  },
+
+  //does the opposite of toBoardCoordinate
+  toPixelCoordinate: function(boardCoordinate, upperLeftBoardMargin, cellSize)
+  {
+    return (boardCoordinate + 0.5)*(cellSize-1) + upperLeftBoardMargin;
+  },
+
+  //tests whether a given position is inside the board or not
+  //@positionOnBoard: array of coordinates as column and row position
+  isInGrid: function(point, gridSize)
+  {
+    return (point[0] >= 0) && (point[0] < gridSize[0]) && (point[1] >= 0) && (point[1] < gridSize[1]);        
+  },
+
+  gridCellAtPoint: function(grid, point) {
+    if(point === null) return null;
+
+    var gridPos = [
+      Math.floor((point[0] - grid.upperLeft[0]) / grid.cellSize[0]),
+      Math.floor((point[1] - grid.upperLeft[1]) / grid.cellSize[1])
+    ];
+
+    if(gridPos[0] < 0 || gridPos[0] > grid.gridSize[0] || gridPos[1] < 0 || gridPos[1] > grid.gridSize[1]) 
+      return null;
+    else
+      return gridPos;
+  },
+
+  gridCellToPoint: function(grid, cell, proportions) {
+    return [
+      (cell[0] + proportions[0]) * grid.cellSize[0] + grid.upperLeft[0], 
+      (cell[1] + proportions[1]) * grid.cellSize[1] + grid.upperLeft[1]
+    ];
+  },
+
+  gridCellUpperLeft: function(grid, cell) { return this.gridCellToPoint(grid, cell, [0, 0]); },
+
+  gridCellCenter: function(grid, cell) { return this.gridCellToPoint(grid, cell, [0.5, 0.5]); },
+
+  // the meta is optional
+  gridCellRectangle: function(grid, cell, meta) {
+    return {
+      type: "rectangle",
+      position: this.gridCellUpperLeft(grid, cell),
+      size: grid.cellSize,
+      meta: meta
+    };
+  },
+
+  gridSizeInPixels: function(grid) {
+    return [grid.cellSize[0] * grid.gridSize[0], grid.cellSize[1] * grid.gridSize[1]];
+  },
+
+
+  calculateRotationAngle: function (center, mousePosition) {
+    var h = [mousePosition[0] - center[0], mousePosition[1] - center[1]];
+    var omDistance = Math.sqrt(h[0]*h[0] + h[1]*h[1]);
+    var ratio = -h[1]/omDistance;
+    var angle = 0;
+    if(omDistance !== 0) {
+      var absValueAngle = Math.acos(ratio)*180/Math.PI;
+      if(h[0] <= 0) {
+        angle = -absValueAngle;
+      } else {
+        angle = absValueAngle;
+      }
+    }
+    return angle;
+  },
+
+  calculateRotationOffset: function(rotation, center, mousePosition) {
+    return rotation - this.calculateRotationAngle(center, mousePosition);
+  },
+
+  calculateRotation: function(rotationOffset, center, mousePosition) {
+    return this.calculateRotationAngle(center, mousePosition) + rotationOffset;
+  },
+
+  // Returns an array containing the index of the first child that is equal to the correct value, or an empty array
+  childByName: function(children, value) {
+    var childIndex = GE.indexOfEquals(children, value);
+    return childIndex != -1 ? [childIndex] : []; 
+  },
+
+  makeFilledRectangle: function(grid, cell, meta) {
+    return _.extend(this.gridCellRectangle(grid, cell, meta), {
+      strokeStyle: "white",
+      fillStyle: "white"
+    });
+  }
+})
